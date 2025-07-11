ecee7a85f69dbe2ba1fe39b522f5b0d460bf2c07
Paresh Adhia <padhia@gmail.com>
2025-07-11 17:30:06 -0400

    switched to using uv

--- flake.nix
+++ flake.nix
@@ -1,40 +1,32 @@
 {
-  description = "make4db provier for DuckDB";
-
   inputs = {
     nixpkgs.url     = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    nix-utils.url = "github:padhia/nix-utils";
-    nix-utils.inputs.nixpkgs.follows = "nixpkgs";
-
     make4db-api.url = "github:padhia/make4db-api";
-    make4db-api.inputs = {
-      nixpkgs.follows = "nixpkgs";
-      flake-utils.follows = "flake-utils";
-      nix-utils.follows = "nix-utils";
-    };
+
+    make4db-api.inputs.nixpkgs.follows = "nixpkgs";
+    make4db-api.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nix-utils, make4db-api }:
+  outputs = { self, nixpkgs, flake-utils, make4db-api }:
   let
-    inherit (nix-utils.lib) pyDevShell;
     inherit (nixpkgs.lib) composeManyExtensions;
 
-    pkgOverlay = final: prev: {
-      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-        (py-final: py-prev: {
-          make4db-duckdb = py-final.callPackage ./make4db-duckdb.nix {};
-        })
-      ];
-    };
-
-    overlays.default = composeManyExtensions [
+    overlays.default =
+    let
+      pkgOverlay = final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (py-final: py-prev: {
+            make4db-duckdb = py-final.callPackage ./make4db-duckdb.nix {};
+          })
+        ];
+      };
+    in composeManyExtensions [
       make4db-api.overlays.default
       pkgOverlay
     ];
 
-    buildSystem = system:
+    eachSystem = system:
     let
       pkgs = import nixpkgs {
         inherit system;
@@ -42,18 +34,26 @@
         overlays = [ self.overlays.default ];
       };
 
-      devShells.default = pyDevShell {
-        inherit pkgs;
-        name = "make4db-duckdb";
-        extra = [
-          "duckdb"
-          "make4db-api"
+      pyPkgs = pkgs.python312Packages;
+
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "m4db-duck";
+        venvDir = "./.venv";
+        buildInputs = [
+          pkgs.ruff
+          pkgs.uv
+          pyPkgs.python
+          pyPkgs.venvShellHook
+          pyPkgs.pytest
+          pyPkgs.duckdb
+          pyPkgs.make4db-api
         ];
       };
-    in { inherit devShells; };
+    };
 
   in {
     inherit overlays;
-    inherit (flake-utils.lib.eachDefaultSystem buildSystem) devShells;
+    inherit (flake-utils.lib.eachDefaultSystem eachSystem) devShells;
   };
 }
